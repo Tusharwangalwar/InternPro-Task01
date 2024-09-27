3094ba87517ed1ad2404b1f3395fa6d1b7cccdb8
Tusharwangalwar <143919392+Tusharwangalwar@users.noreply.github.com>
2024-09-27 22:52:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3027;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.use(express.static('public'));
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
